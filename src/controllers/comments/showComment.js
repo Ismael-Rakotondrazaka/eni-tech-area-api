b859f60c313db2f5c49d7a05a2e1ce644bbb910b
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-03-04 16:52:23 +0300

    Feat: Add comments features

---
+++ src/controllers/comments/showComment.js
@@ -0,0 +1,74 @@
+import { Answer, Comment, Question, User } from "../../models/index.js";
+import { commentResource } from "../../resources/index.js";
+import {
+  UnauthorizedError,
+  createDataResponse,
+  NotFoundError,
+} from "../../utils/index.js";
+
+const showComment = async (req, res, next) => {
+  try {
+    const authUserId = req.payload?.user?.id;
+    const { questionId, answerId, commentId } = req.params;
+
+    if (!authUserId)
+      throw new UnauthorizedError({
+        message: "Credential doesn't match to our records.",
+        code: "E5_1",
+      });
+
+    if (
+      !/^\d+$/.test(questionId) ||
+      !/^\d+$/.test(answerId) ||
+      /^\d+$/.test(commentId)
+    )
+      throw new NotFoundError();
+
+    const authUser = await User.findByPk(authUserId);
+
+    if (!authUser)
+      throw new UnauthorizedError({
+        message: "Credential doesn't match to our records.",
+        code: "E5_1",
+      });
+
+    const targetQuestion = await Question.findByPk(questionId);
+
+    if (!targetQuestion) throw new NotFoundError();
+
+    const targetAnswer = await Answer.findByOne({
+      where: {
+        id: answerId,
+        questionId,
+      },
+    });
+
+    if (!targetAnswer) throw new NotFoundError();
+
+    const targetComment = await Comment.findOne({
+      where: {
+        id: commentId,
+        answerId,
+      },
+    });
+
+    const targetCommentResource = commentResource(targetComment);
+
+    const data = {
+      comment: targetCommentResource,
+    };
+
+    const dataResponse = createDataResponse({
+      data,
+      request: req,
+    });
+
+    res.status(201);
+
+    return res.json(dataResponse);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { showComment };
--- src/resources/index.js
+++ src/resources/index.js
@@ -2,3 +2,5 @@ export * from "./userResource.js";
 export * from "./questionResource.js";
 export * from "./answerResource.js";
 export * from "./answerCollection.js";
+export * from "./commentResource.js";
+export * from "./commentCollection.js";
