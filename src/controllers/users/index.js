403d8389027af513465d68bc9c72b6b70f3ee1fc
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-03-04 21:50:49 +0300

    Feat: Add updateUser feature

--- src/controllers/users/index.js
+++ src/controllers/users/index.js
@@ -1,1 +1,2 @@
 export * from "./showUser.js";
+export * from "./updateUser.js";
--- src/routes/api/v1/users/index.js
+++ src/routes/api/v1/users/index.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { showUser } from "../../../../controllers/index.js";
+import { showUser, updateUser } from "../../../../controllers/index.js";
 import { authMiddleware } from "../../../../middlewares/index.js";
 
 const userRoutes = Router();
 
 userRoutes.get("/:userId", authMiddleware, showUser);
+userRoutes.put("/:userId", authMiddleware, updateUser);
 
 export { userRoutes };
