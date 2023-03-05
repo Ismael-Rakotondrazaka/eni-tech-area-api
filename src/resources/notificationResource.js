d4ecfb1602098bb53652384d172856a4cff3f77a
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-03-05 13:33:51 +0300

    Fix: Fix notification resource type

--- src/resources/notificationResource.js
+++ src/resources/notificationResource.js
@@ -1,6 +1,7 @@
 const notificationResource = (resource) => {
   return {
     id: resource.id,
+    type: resource.type,
     userId: resource.userId,
     content: resource.content,
     createdAt: resource.createdAt,
