520f74d1337858fe59bf865695bc862b7bf281e4
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-03-05 14:54:12 +0300

    Feat: add searchQuestion feature

--- src/controllers/questions/index.js
+++ src/controllers/questions/index.js
@@ -2,3 +2,4 @@ export * from "./storeQuestion.js";
 export * from "./showQuestion.js";
 export * from "./indexQuestion.js";
 export * from "./indexQuestionSuggestion.js";
+export * from "./searchQuestion.js";
