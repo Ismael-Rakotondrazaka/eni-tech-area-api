cb100a5fc040baa852275ac7cc4e088160e69c7a
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-03-09 14:53:35 +0300

    Feat: Create validateEndAt feature

--- src/utils/strings/validateEndAt.js
+++ src/utils/strings/validateEndAt.js
@@ -1,7 +1,17 @@
-const validateEndAt = (endAt ) => {
-    // TODO validate endAt
+import { BadRequestError } from "../errors/index.js";
 
-    return endAt;
+const validateEndAt = (endAt) => {
+  const minTime = 900000; // 15mn
+
+  const now = new Date();
+
+  if (endAt.getTime() - now.getTime() < minTime)
+    throw new BadRequestError({
+      message: "endAt must be in the future (15 min)",
+      code: "E2_",
+    });
+
+  return endAt;
 };
 
 export { validateEndAt };
