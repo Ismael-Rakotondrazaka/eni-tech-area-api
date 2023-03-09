fae9553f70569404975a67ef0faed9bb54ca5d7d
RAGSGAR <ragsgar@localhost.localdomain>
2023-03-09 18:59:23 +0300

    update models event

--- src/models/Event.js
+++ src/models/Event.js
@@ -30,6 +30,10 @@ export default (sequelize, DataTypes) => {
         type: DataTypes.TEXT,
         allowNull: false,
       },
+      image: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      },
       startAt: {
         type: DataTypes.DATE,
         allowNull: true,
