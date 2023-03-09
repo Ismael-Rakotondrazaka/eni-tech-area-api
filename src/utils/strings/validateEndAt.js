27841e6096a99c1f008911f865df1e5cb57cdc41
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-03-09 15:12:43 +0300

    Fix: add more validation to validateEndAt

--- src/utils/strings/validateEndAt.js
+++ src/utils/strings/validateEndAt.js
@@ -1,11 +1,25 @@
 import { BadRequestError } from "../errors/index.js";
 
 const validateEndAt = (endAt) => {
+  if (!endAt)
+    throw new BadRequestError({
+      message: "endAt is required.",
+      code: "E2_",
+    });
+
   const minTime = 900000; // 15mn
 
   const now = new Date();
 
-  if (endAt.getTime() - now.getTime() < minTime)
+  const toCompare = new Date(endAt);
+
+  if (isNaN(toCompare.getTime()))
+    throw new BadRequestError({
+      message: "endAt is not a valid date.",
+      code: "E2_",
+    });
+
+  if (toCompare.getTime() - now.getTime() < minTime)
     throw new BadRequestError({
       message: "endAt must be in the future (15 min)",
       code: "E2_",
