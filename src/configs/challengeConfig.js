dc99e7d9d8bb59cdce217575e44fb7b53517e16f
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-03-10 12:48:16 +0300

    Feat: Create challengeConfig

---
+++ src/configs/challengeConfig.js
@@ -0,0 +1,13 @@
+const challengeConfig = {
+  CHALLENGE_STATUS: ["success", "failure", "pending"],
+  DEFAULT_CHALLENGE_STATUS: "pending",
+  VALID_CHALLENGE_STATUS: ["success", "failure"],
+  CHALLENGE_DIFFICULTY: ["easy", "medium", "difficult"],
+  CHALLENGE_DIFFICULTY_POINT: {
+    EASY: 5,
+    MEDIUM: 10,
+    DIFFICULT: 15,
+  },
+};
+
+export { challengeConfig };
