0a6bb06a469fdf9257cd923f52d5d508081b4385
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-05-26 18:43:32 +0300

    feat: add support for hashed password in the user seeder

--- src/seeders/20230523112824-create-users.js
+++ src/seeders/20230523112824-create-users.js
@@ -1,5 +1,9 @@
 "use strict";
 const { faker } = require("@faker-js/faker");
+const { hashSync } = require("bcrypt");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
@@ -10,7 +14,7 @@ module.exports = {
         firstname: "John",
         lastname: "Doe",
         email: "john@doe.com",
-        password: "00000000",
+        password: hashSync("password", +process.env.PASSWORD_SALT_ROUND),
         role: "user",
         imageUrl: "https://i.pravatar.cc/300?u=1",
       },
@@ -19,7 +23,7 @@ module.exports = {
         firstname: "Jane",
         lastname: "Smith",
         email: "jane@smith.com",
-        password: "00000000",
+        password: hashSync("password", +process.env.PASSWORD_SALT_ROUND),
         role: "admin",
         imageUrl: "https://i.pravatar.cc/300?u=2",
       },
@@ -31,7 +35,7 @@ module.exports = {
         firstname: faker.name.firstName(),
         lastname: faker.name.lastName(),
         email: faker.internet.email(),
-        password: "00000000",
+        password: hashSync("password", +process.env.PASSWORD_SALT_ROUND),
         role: faker.helpers.arrayElement(["user", "admin"]),
         imageUrl: "https://i.pravatar.cc/300?u=" + i,
       });
